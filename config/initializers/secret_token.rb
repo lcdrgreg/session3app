a4af148ec3d5b61f77742aff40326c131caf6ca8
Gregory A Teschner <lcdrgreg@gmail.com>
2013-04-29 18:38:27 -0500

    adding rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Session3app::Application.config.secret_token = '9bb289c2f86f289fdb8b3baa4d586edade882725245c9e2a2ec98f06f44ee514f866597f86f52cb24019ed3e214b6bd40ffb72fae045e6e16dc80dbd8d633f83'
